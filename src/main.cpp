da4f47fa2648218b0504e278d3efff06ad81e760
Lukasz <lukasz.sromek@gmail.com>
2020-11-08 12:53:22 +0100

    basic project structure

---
+++ src/main.cpp
@@ -0,0 +1,28 @@
+
+#include <wx/wx.h>
+
+class MyApp : public wxApp
+{
+public:
+    virtual bool OnInit();
+};
+
+wxIMPLEMENT_APP(MyApp);
+
+class MyFrame : public wxFrame
+{
+public:
+    MyFrame(const wxString &title, const wxPoint &pos, const wxSize &size);
+};
+
+bool MyApp::OnInit()
+{
+    MyFrame *frame = new MyFrame("Hello World", wxPoint(50, 50), wxSize(450, 340));
+    frame->Show(true);
+    return true;
+}
+
+MyFrame::MyFrame(const wxString &title, const wxPoint &pos, const wxSize &size)
+    : wxFrame(NULL, wxID_ANY, title, pos, size)
+{
+}
