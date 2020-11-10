40de99300c072cc0927dd6c274f358e141fee984
Lukasz <lukasz.sromek@gmail.com>
2020-11-10 18:06:05 +0100

    starting point for splitters

--- src/main.cpp
+++ src/main.cpp
@@ -23,25 +23,4 @@ bool MyApp::OnInit()
 
 MyFrame::MyFrame(const wxString &title, const wxPoint &pos, const wxSize &size) : wxFrame(nullptr, wxID_ANY, title, pos, size)
 {
-    wxPanel *panel_top = new wxPanel(this, wxID_ANY, wxDefaultPosition, wxSize(200, 100));
-    panel_top->SetBackgroundColour(wxColor(100, 100, 200));
-
-    wxPanel *panel_bottom = new wxPanel(this, wxID_ANY, wxDefaultPosition, wxSize(200, 100));
-    panel_bottom->SetBackgroundColour(wxColor(100, 200, 100));
-
-    wxPanel *panel_bottom_right = new wxPanel(panel_bottom, wxID_ANY, wxDefaultPosition, wxSize(200, 100));
-    panel_bottom_right->SetBackgroundColour(wxColor(200, 200, 100));
-
-    wxBoxSizer *sizer = new wxBoxSizer(wxVERTICAL);
-    sizer->Add(panel_top, 1, wxEXPAND | wxLEFT | wxTOP | wxRIGHT, 10);
-    sizer->Add(panel_bottom, 1, wxEXPAND | wxALL, 10);
-
-    wxSizer *sizer_bottom = new wxBoxSizer(wxVERTICAL);
-    wxSizer *sizer_centering = new wxBoxSizer(wxHORIZONTAL);
-    sizer_centering->Add(panel_bottom_right, 0, wxALIGN_CENTER);
-    sizer_bottom->Add(sizer_centering, 1, wxALIGN_CENTER);
-
-    panel_bottom->SetSizerAndFit(sizer_bottom);
-
-    this->SetSizerAndFit(sizer);
 }
