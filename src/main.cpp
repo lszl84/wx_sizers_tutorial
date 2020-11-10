8c9ce0d4769e663a21317d5bb7cee97013200974
Lukasz <lukasz.sromek@gmail.com>
2020-11-10 15:05:40 +0100

    two panels

--- src/main.cpp
+++ src/main.cpp
@@ -23,11 +23,15 @@ bool MyApp::OnInit()
 
 MyFrame::MyFrame(const wxString &title, const wxPoint &pos, const wxSize &size) : wxFrame(nullptr, wxID_ANY, title, pos, size)
 {
-    wxPanel *panel = new wxPanel(this, wxID_ANY, wxDefaultPosition, wxSize(200, 100));
-    panel->SetBackgroundColour(wxColor(100, 100, 200));
+    wxPanel *panel_top = new wxPanel(this, wxID_ANY, wxDefaultPosition, wxSize(200, 100));
+    panel_top->SetBackgroundColour(wxColor(100, 100, 200));
+
+    wxPanel *panel_bottom = new wxPanel(this, wxID_ANY, wxDefaultPosition, wxSize(200, 100));
+    panel_bottom->SetBackgroundColour(wxColor(100, 200, 100));
 
     wxBoxSizer *sizer = new wxBoxSizer(wxVERTICAL);
-    sizer->Add(panel, 1, wxEXPAND | wxLEFT, 10);
+    sizer->Add(panel_top, 1, wxEXPAND | wxLEFT | wxTOP | wxRIGHT, 10);
+    sizer->Add(panel_bottom, 2, wxEXPAND | wxALL, 10);
 
     this->SetSizerAndFit(sizer);
 }
